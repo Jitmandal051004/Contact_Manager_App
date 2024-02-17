abe927d96404b2dd72c895e58fcddf04a62eab1e
indrajit-mandal <102886330+Jitmandal051004@users.noreply.github.com>
2024-02-17 16:23:30 +0530

    errorhandling going on

--- myContacts-backend/controllers/contactController.js
+++ myContacts-backend/controllers/contactController.js
@@ -9,6 +9,12 @@ const getContacts = (req,res) => {
 //@route CREATE /api/contacts
 //@access public 
 const createContact = (req,res) => {
+    console.log("The request Body is :", req.body);
+    const {name, email, phone}=req.body;
+    if(!name || !email || !phone){
+        res.status(400);
+        throw new Error("All fields are mandatory!!");
+    }
     res.status(201).json({message: "Create Contact"});
 }
 
@@ -16,21 +22,27 @@ const createContact = (req,res) => {
 //@route GET /api/contacts/:id
 //@access public 
 const getContact = (req,res) => {
-    res.status(202).json({message: `Get Contact for ${req.params.id}`});    res.status(200).json({message: `Get Contact for ${req.params.id}`});
+    res.status(200).json({message: `Get Contact for ${req.params.id}`});    res.status(200).json({message: `Get Contact for ${req.params.id}`});
 }
 
 //@desc update contact
 //@route UPDATE /api/contacts/:id
 //@access public 
 const updateContact = (req,res) => {
-    res.status(203).json({message: `Update contact for ${req.params.id}`});
+    res.status(200).json({message: `Update contact for ${req.params.id}`});
 }
 
 //@desc delete contact
 //@route DELETE /api/contacts/:id
-//@access public 
+//@access public  
 const deleteContact = (req,res) => {
-    res.status(204).json({message: `Delete contact for ${req.params.id}`});
+    res.status(200).json({message: `Delete contact for ${req.params.id}`});
 }
 
-module.exports = { getContacts , createContact , getContact , updateContact , deleteContact };
+module.exports = { 
+    getContacts,
+    createContact, 
+    getContact, 
+    updateContact, 
+    deleteContact
+};
