8dbf073b6e8fc36cb6ac5c08bc7c9311c7a3e94d
indrajit-mandal <102886330+Jitmandal051004@users.noreply.github.com>
2024-02-20 06:40:11 +0530

    app is completed

--- myContacts-backend/routes/contactRoutes.js
+++ myContacts-backend/routes/contactRoutes.js
@@ -6,9 +6,10 @@ const {
     getContacts, 
     updateContact, 
     deleteContact } = require("../controllers/contactController");
+const validateToken = require("../middleware/validTokenHandler");
 
+router.use(validateToken);
 router.route("/").get(getContacts).post(createContact);
-
 router.route("/:id").get(getContact).put(updateContact).delete(deleteContact);
 
 module.exports = router;
